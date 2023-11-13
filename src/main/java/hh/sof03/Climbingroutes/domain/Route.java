07327e45db9a79915599a52890ccb38198580f67
ArttuuS <arttu.saily@gmail.com>
2023-11-14 01:27:43 +0200

    delete works again

--- src/main/java/hh/sof03/Climbingroutes/domain/Route.java
+++ src/main/java/hh/sof03/Climbingroutes/domain/Route.java
@@ -1,13 +1,17 @@
 package hh.sof03.Climbingroutes.domain;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Route {
@@ -28,6 +32,9 @@ public class Route {
 	@JsonIgnoreProperties("routesS")
 	@JoinColumn(name = "disciplineid")
 	private Discipline discipline;
+	
+	@OneToMany(mappedBy = "route", cascade = CascadeType.ALL)
+    private List<ClimbingLog> climbingLogs;
 
 	public Route() {
 
